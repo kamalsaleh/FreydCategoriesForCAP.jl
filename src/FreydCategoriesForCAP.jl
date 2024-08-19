a4eef6019f8253aa81aa0bd60270c956ebe61368
Fabian Zickgraf <f.zickgraf@dashdos.com>
2024-08-19 11:53:27 +0200

    Import all operations in Base and in CAP packages by default

--- src/FreydCategoriesForCAP.jl
+++ src/FreydCategoriesForCAP.jl
@@ -2,46 +2,13 @@ module FreydCategoriesForCAP
 
 @nospecialize
 
-import Base./
-import Base.*
-import Base.^
-import Base.==
-import Base.getindex
-
 using MatricesForHomalg
 
 using CAP
 
-const IsOne = CAP.IsOne # MatricesForHomalg also exports IsOne
-
-import CAP.DistinguishedObjectOfHomomorphismStructureExtendedByFullEmbedding
-import CAP.HomomorphismStructureOnObjectsExtendedByFullEmbedding
-import CAP.HomomorphismStructureOnMorphismsExtendedByFullEmbedding
-import CAP.HomomorphismStructureOnMorphismsWithGivenObjectsExtendedByFullEmbedding
-import CAP.InterpretMorphismAsMorphismFromDistinguishedObjectToHomomorphismStructureExtendedByFullEmbedding
-import CAP.InterpretMorphismAsMorphismFromDistinguishedObjectToHomomorphismStructureWithGivenObjectsExtendedByFullEmbedding
-import CAP.InterpretMorphismFromDistinguishedObjectToHomomorphismStructureAsMorphismExtendedByFullEmbedding
-
-import CAP.ModelingTowerObjectConstructor
-import CAP.ModelingTowerObjectDatum
-import CAP.ModelingTowerMorphismConstructor
-import CAP.ModelingTowerMorphismDatum
-import CAP.ModelingObject
-import CAP.ReinterpretationOfObject
-import CAP.ModelingMorphism
-import CAP.ReinterpretationOfMorphism
-
 using MonoidalCategories
 
 using LinearAlgebraForCAP # this is not a GAP dependency but needed for various declarations not (yet) part of MatricesForHomalg
-
-import LinearAlgebraForCAP.NrRows
-import LinearAlgebraForCAP.NrCols
-
-#import CAP.StringGAP_OPERATION
-#import CAP.ViewString
-#import CAP.DisplayString
-#import CAP.LaTeXOutput
 
 include("init.jl")
 
