453e342fe7333d8e155f90bf3d75073bff44a726
Fabian Zickgraf <f.zickgraf@dashdos.com>
2024-08-26 16:00:21 +0200

    Replace IS_PRECOMPILING by the library function `generating_output`
    introduced in Julia 1.11

--- ext/FreydCategoriesForCAPFinSetsForCAPExtension.jl
+++ ext/FreydCategoriesForCAPFinSetsForCAPExtension.jl
@@ -10,8 +10,6 @@ using FinSetsForCAP
 
 @IMPORT_THE_WORLD()
 
-CAP.IS_PRECOMPILING = true
-
 push!(ModulesForEvaluationStack, @__MODULE__)
 
 #include("../src/gap/GroupsAsCats.gi.autogen.jl") # groups are not yet supported
@@ -19,7 +17,5 @@ include("../src/gap/LinearClosure.gi.autogen.jl")
 include("../src/gap/ProSetsAsCats.gi.autogen.jl")
 
 push!(ModulesForEvaluationStack)
-
-CAP.IS_PRECOMPILING = false
 
 end # module FreydCategoriesForCAPFinSetsForCAPExtension
